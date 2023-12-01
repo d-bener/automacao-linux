3e9b2774603e3aaf2c8a55057f000b6915ca78fe
d-bener <103961472+d-bener@users.noreply.github.com>
2023-12-01 14:25:48 -0300

    Add files via upload

---
+++ chamados.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# captura posicao do mouse xdotool getmouselocation
+
+
+xdotool mousemove 605 414 click 1
+sleep 5 
+
+xdotool mousemove 336 232 click 1
+sleep 1 
+xdotool mousemove 1139 336 click 1
+sleep 1 
+
+xdotool mousemove 1647 362 click 1
+sleep 1
+xdotool key Ctrl+a
+sleep 1
+xdotool type "normalizado!"
+sleep 1 
+
+xdotool mousemove 363 605 click 1
+sleep 1  
+xdotool key Page_Down
+sleep 1
+xdotool mousemove 345 1045 click 1
+sleep 2
+xdotool mousemove 114 1009 click 1
+sleep 1
+
+xdotool mousemove 1659 426 click 1
+sleep 1
+xdotool type "res"
+sleep 1
+xdotool mousemove 1609 459 click 1
+sleep 1
+
+xdotool mousemove 1808 420 click 1
+sleep 1 
+
+xdotool mousemove 413 535 click 1
+sleep 3 
+
+xdotool type "sem"
+sleep 1 
+xdotool mousemove 387 594 click 1
+sleep 1 
+
+xdotool mousemove 220 605 click 1
+sleep 1
+
+xdotool type "infra"
+sleep 3
+
+xdotool mousemove 121 664 click 1
+sleep 2
+
+
+xdotool mousemove 1808 420 click 1
+
+sleep 3
+
+xdotool mousemove 278 113 click 1
+sleep 5 
+
+
+xdotool key F5
+
+sleep 5 
